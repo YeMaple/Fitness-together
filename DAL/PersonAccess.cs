857215884f5d8e59152377374c1ec886715be728
YeMaple <gmapleleaf@gmail.com>
2018-04-19 16:05:43 -0700

    Added a few Accesses

---
+++ DAL/PersonAccess.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DAL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL
+{
+    class PersonAccess
+    {
+        // WARNING!!! NEEDED TO BE CHANGED
+        private readonly POCContext _context;
+
+        public PersonAccess(POCContext context)
+        {
+            _context = context;
+        }
+
+        ////////////////////////READER////////////////////
+
+        public M_Person GetPersonByLogin(string email, string password)
+        {
+            return _context.Person.FirstOrDefault(p => p.Email == email && p.Password == password);
+        }
+
+        public T GetPersonById<T>(int id) where T : class
+        {
+            var dbSet = _context.Set<T>();
+            var result = dbSet.Find(id);
+            return (T)Convert.ChangeType(result, typeof(T));
+        }
+
+        ////////////////////////CREATER////////////////////
+
+        public T CreatePerson<T>(T p_obj) where T : class
+        {
+            var dbSet = _context.Set<T>();
+            dbSet.Add(p_obj);
+            _context.SaveChanges();
+            return p_obj;
+        }
+
+        public void DeletePerson<T>(int id) where T : class
+        {
+            var dbSet = _context.Set<T>();
+            var deleteMe = dbSet.Find(id);
+            _context.Remove(deleteMe);
+            _context.SaveChanges();
+        }
+
+        public T UpdatePerson<T>(T obj, int id) where T : class
+        {
+            var dbSet = _context.Set<T>();
+            var updateMe = dbSet.Find(id);
+            if (updateMe == null)
+            {
+                return obj;
+            }
+
+            dbSet.Update(obj);
+            _context.SaveChanges();
+            return updateMe;
+        }
+    }
+}
---
+++ DAL/WorkoutAccess.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DAL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL
+{
+    class WorkoutAccess
+    {
+        // WARNING!!! NEEDED TO BE CHANGED
+        private readonly POCContext _context;
+
+        public WorkoutAccess(POCContext context)
+        {
+            _context = context;
+        }
+
+        ////////////////////////READER////////////////////
+
+        public M_Workout GetPersonWorkouts(int creator_id)
+        {
+            return _context.Workout.Find(w => w.Creator_id == creator_id);
+        }
+
+        public T GetWorkoutById<T>(int id) where T : class
+        {
+            var dbSet = _context.Set<T>();
+            var result = dbSet.Find(id);
+            return (T)Convert.ChangeType(result, typeof(T));
+        }
+
+        public M_Workout GeWorkoutsBySearch(string name, string category)
+        {
+            return _context.Workout.Where(w => w.Name.Contains(name) && w.Category.Contains(category));
+        }
+
+        ////////////////////////CREATER////////////////////
+
+        public T CreateWorkout<T>(T w_obj) where T : class
+        {
+            var dbSet = _context.Set<T>();
+            dbSet.Add(w_obj);
+            _context.SaveChanges();
+            return w_obj;
+        }
+
+        ////////////////////////DELETE////////////////////
+
+        public void DeleteWorkout<T>(int id) where T : class
+        {
+            var dbSet = _context.Set<T>();
+            var deleteMe = dbSet.Find(id);
+            _context.Remove(deleteMe);
+            _context.SaveChanges();
+        }
+
+        ////////////////////////UPDATE////////////////////
+
+        public T UpdateWorkout<T>(T obj, int id) where T : class
+        {
+            var dbSet = _context.Set<T>();
+            var updateMe = dbSet.Find(id);
+            if (updateMe == null)
+            {
+                return obj;
+            }
+
+            dbSet.Update(obj);
+            _context.SaveChanges();
+            return updateMe;
+        }
+    }
+}
